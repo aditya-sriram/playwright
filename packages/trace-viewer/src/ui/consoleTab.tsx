1b936b74b5b7ec74ced123d4f3b2c6d57b8cd29d
Aditya Sriram <asriram@umich.edu>
2023-04-15 15:50:44 -0400

    added test

--- packages/trace-viewer/src/ui/consoleTab.tsx
+++ packages/trace-viewer/src/ui/consoleTab.tsx
@@ -19,7 +19,7 @@ import type { ActionTraceEvent } from '@trace/trace';
 import * as React from 'react';
 import './consoleTab.css';
 import * as modelUtil from './modelUtil';
-import { vsprintf } from "printj";
+import { formatString } from './consoleLog';
 
 export const ConsoleTab: React.FunctionComponent<{
   action: ActionTraceEvent | undefined,
@@ -86,23 +86,3 @@ function iconClass(message: channels.ConsoleMessageInitializer): string {
   return 'blank';
 }
 
-function formatString(msg: string | undefined): string | undefined {
-  if (!msg) return msg;
-  var match = /(\%.)(?!.*\1)/.exec(msg);
-  if (match) {
-      var idx = match.index + 2;
-      var substitution_section = msg.substring(0, idx).trim();
-      var substitution = msg.substring(idx).trim();
-      var substitution_matches = msg.match(/\%./g);
-      if (substitution_matches) {
-        var count = substitution_matches.length;
-        var substitution_split = substitution.split(" ")
-        var valid = count == substitution_split.length;
-        console.log(valid);
-        if (valid) {
-          msg = vsprintf(substitution_section, substitution_split);
-        }
-      }
-  }
-  return msg;
-}
